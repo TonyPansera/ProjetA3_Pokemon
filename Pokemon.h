800e4e99d20144e9c9884978309b57867e092ca3
Tony Pansera <tonypansera9@gmail.com>
2025-04-15 16:41:13 +0200

    test commit

--- Pokemon.h
+++ Pokemon.h
@@ -1,6 +1,16 @@
+
 #ifndef POKEMON_H
 #define POKEMON_H
 
+#include <string>
+using namespace std;
 
+class Pokemon {
+    protected: 
+        string nom;
+        int pv;
+        string attaque;
+        int degatsAttaque;
+};
 
 #endif
