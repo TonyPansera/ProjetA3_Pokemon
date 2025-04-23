1980c92f0fdcc866c2a7c302f079684cfd34ca79
Tony Pansera <tonypansera9@gmail.com>
2025-04-23 13:31:34 +0200

    controller v1

---
+++ Controller/PokemonController.h
@@ -0,0 +1,35 @@
+#ifndef POKEMONCONTROLLER_H
+#define POKEMONCONTROLLER_H
+
+#include <string>
+#include "../Model/PokemonTypes.h"
+#include "../Model/Joueur.h"
+#include "../Model/Maitre.h"
+#include "../Model/Leader.h"
+
+using namespace std;
+
+class PokemonController {
+    private:
+        vector<PokemonComplet> alPokemons;
+        vector<Joueur> alJoueurs;
+        vector<Maitre> alMaitres;
+        vector<Leader> alLeaders;
+
+        Type* alTypes[17];
+
+        void typesInit();
+        void addAllPokemons();
+        void addJoueurs();
+        void addMaitres();
+        void addLeaders();
+
+
+    
+    public:
+        PokemonController();
+        
+        
+};
+
+#endif
